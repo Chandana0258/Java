e5d8b74610998d4d2684a76856a01fd1ca0cccbb
Chandana0258 <chandanaachandu16@gmail.com>
2025-09-28 11:08:03 +0530

    pushing files

---
+++ src/PrefixAutocomplete.java
@@ -0,0 +1,17 @@
+import java.util.NavigableSet;
+import java.util.Scanner;
+import java.util.SortedSet;
+
+public class PrefixAutocomplete {
+    public static void main(String[] args) {
+        public static SortedSet<String> byPrefix(NavigableSet<String> dict, String prefix) {
+            String word = prefix + Character.MAX_VALUE;
+            return dict.subSet(Prefix, true, hi, true);
+        }
+
+public static  void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            NavigableSet 
+        }
+    }
+}
